62ea1eb08d2430ff7220a0859fec0fb9d16da170
Mike Devresse <mike.devresse@gmail.com>
2021-02-18 18:55:00 +0100

    Implementing getjwthandler

--- src/MessageHandler/Authentication/GetJWTHandler.php
+++ src/MessageHandler/Authentication/GetJWTHandler.php
@@ -4,11 +4,38 @@
 namespace App\MessageHandler\Authentication;
 
 
+use App\Entity\User;
+use App\Exception\Authentication\BadCredentialsException;
+use App\Exception\Authentication\UserNotFoundException;
 use App\MessageHandler\AbstractHandler;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class GetJWTHandler extends AbstractHandler
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordEncoderInterface $passwordEncoder,
+        private JWTEncoderInterface $JWTEncoder
+    )
+    {
+    }
+
     public function __invoke(array $data)
     {
+        $this->checkFieldsMissing($data,['username','password']);
+
+        /** @var User $user */
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username'=>$data['username']]);
+
+        if(!$user) throw new UserNotFoundException($data['username']);
+
+        if(!$this->passwordEncoder->isPasswordValid($user,$data['password'])) throw new BadCredentialsException();
+
+        return [
+            'code' => 200,
+            'token' => $this->JWTEncoder->encode(['username'=>$data['username']])
+        ];
     }
 }
